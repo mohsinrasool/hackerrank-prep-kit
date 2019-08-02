5ccb19733ad9790c060b38ff4415e5da1d29c184
Mohsin Rasool <mohsin.rasool@gmail.com>
2019-08-03 04:13:42 +0500

    Left rotation of array

---
+++ Arrays/2-array-left-rotation.php
@@ -0,0 +1,41 @@
+<?php
+
+// Complete the rotLeft function below.
+function rotLeft($a, $d) {
+
+    $n = count($a);
+    $d = $d % $n;
+    $c = 0;
+
+    $result = array();
+    for($i=$d; $i<$n; $i++) {
+        $result[$c++] = $a[$i];
+    }
+    for($i=0; $i<$d; $i++) {
+        $result[$c++] = $a[$i];
+    }
+
+    return $result;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%[^\n]", $nd_temp);
+$nd = explode(' ', $nd_temp);
+
+$n = intval($nd[0]);
+
+$d = intval($nd[1]);
+
+fscanf($stdin, "%[^\n]", $a_temp);
+
+$a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = rotLeft($a, $d);
+
+fwrite($fptr, implode(" ", $result) . "\n");
+
+fclose($stdin);
+fclose($fptr);
