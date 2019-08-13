23ab7cb5b0606f90493a5cd1fc751255d5bbe89e
Mohsin Rasool <mohsin.rasool@gmail.com>
2019-08-13 18:02:33 +0500

    Minor changes

--- Sorting/1-mark-and-toys.php
+++ Sorting/1-mark-and-toys.php
@@ -8,14 +8,21 @@ function maximumToys($prices, $k) {
     $tmp = 0;
 
     for( $i=0; $i < $size; $i++ ) {
+        
+        // loop starts from next element
         for( $j=$i; $j < $size; $j++ ) {
+        
             if( $prices[$j] < $prices[$i] ) {
+
+                // swap prices
                 $tmp = $prices[$j];
                 $prices[$j] = $prices[$i];
                 $prices[$i] = $tmp;
             }
         }
+
         $sum += $prices[$i];
+        
         if( $sum > $k ) {
             echo "sum = " . $sum . " | k = " . $k . "\n";
             print_r($prices);
@@ -25,50 +32,6 @@ function maximumToys($prices, $k) {
 
     return $i;
 
-/*
-    $prices = mergeSort($prices);
-
-    $sum = 0;
-    for($i=0; $i < count($prices); $i++) {
-        $sum += $prices[$i];
-        if( $sum >= $k )
-            break;
-    }
-
-    return $i;
-    */
-}
-
-function mergeSort($my_array){
-    if(count($my_array) == 1 ) return $my_array;
-    $mid = count($my_array) / 2;
-    $left = array_slice($my_array, 0, $mid);
-    $right = array_slice($my_array, $mid);
-    $left = mergeSort($left);
-    $right = mergeSort($right);
-    return merge($left, $right);
-}
-
-function merge($left, $right){
-    $res = array();
-    while (count($left) > 0 && count($right) > 0){
-        if($left[0] > $right[0]){
-            $res[] = $right[0];
-            $right = array_slice($right , 1);
-        }else{
-            $res[] = $left[0];
-            $left = array_slice($left, 1);
-        }
-    }
-    while (count($left) > 0){
-        $res[] = $left[0];
-        $left = array_slice($left, 1);
-    }
-    while (count($right) > 0){
-        $res[] = $right[0];
-        $right = array_slice($right, 1);
-    }
-    return $res;
 }
 
 
